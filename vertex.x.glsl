2ee014b47a698b6b7deab1bfa59868f4516acdaa
vivichrist <vivichrist@gmail.com>
2015-08-29 14:36:44 +1200

    fixed phong it was in The GLSL Cookbook...

--- vertex.x.glsl
+++ vertex.x.glsl
@@ -10,23 +10,24 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 texCoord;
 
-smooth out vec3 vLightDir;
 smooth out vec2 vUV;
-smooth out vec3 vNormal; //interpolate the Normal
+smooth out vec3 vNormal;
+smooth out vec3 vView;
+smooth out vec3 vLightDir;
 
 void main(void)
 {
 	// Get surface normal in eye coordinates
 	vNormal = normalize(normM * normal);
 	vUV = texCoord;
-
 	// Get vertex position in eye coordinates
 	vec4 pos4 = mvM * vec4( position, 1 );
-	vec3 eye3 = pos4.xyz / pos4.w;
-
-	// Get vector to light source
-	vLightDir = normalize(lightP - eye3);
-
-	// Don’t forget to transform the geometry!
+	vView = pos4.xyz / pos4.w;
+	// Get light position in eye coordinates
+	vec4 lgt4 = mvM * vec4( lightP, 1 );
+    vec3 lpos = lgt4.xyz / lgt4.w;
+	
+	vLightDir = normalize(lpos - vView);
+	// transform the geometry!
 	gl_Position = projM * pos4;
 }
