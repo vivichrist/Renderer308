3c4741793264431ec49bc4e05c7744f10a5f219a
Vivian Stewart <vivichrist@gmail.com>
2015-09-27 16:41:37 +1300

    files to add

---
+++ fragment_cmap.glsl
@@ -0,0 +1,75 @@
+// fragment shader for simple Phong Lighting model
+#version 330
+#define MAX_LIGHTS 10
+
+out vec4 FBColor;
+
+in VertexData {
+	smooth in vec2 vUV;
+	smooth in vec3 vNormal;
+	smooth in vec3 vView;
+} finout;
+
+uniform vec4 material; // intensities, shininess is w
+uniform sampler2D image;
+
+uniform struct Light {
+	vec4 pos;
+	vec4 spec; // color of specular highlights
+	vec4 att; // attenuation coefficients, ambient coefficient is w;
+	vec4 coneDir; // cone direction, cone angle is w;
+} allLights[MAX_LIGHTS];
+
+uniform int numLights;
+
+vec3 lighting( Light l, vec3 diffuse, vec3 normal, vec3 pos, vec3 eye )
+{
+    vec3 lightDir; // vector from surface to the light
+    float att = 1.0; // attenuation
+
+    if ( l.pos.w == 0.0 ) // directional light
+    {
+        lightDir = normalize(l.pos.xyz);
+        att = 1.0; // no attenuation for directional lights
+    } 
+    else
+    {   // point light
+        vec4 lgt4 = finout.mvM * l.pos;
+        lightDir = (lgt4.xyz / lgt4.w) - pos;
+        float dist = length(lightDir);
+        lightDir = normalize(lightDir);
+        // spotlight cone
+        float inside = degrees(acos(dot(-eye, normalize(l.coneDir.xyz))));
+        if ( inside > l.coneDir.w )
+        {
+            att = 0.0;
+        }
+        else att = 1.0/(l.att.x + (l.att.y*dist) + (l.att.z*dist*dist));
+    }
+    vec3 mat = diffuse.rgb * material.xyz; // matte colour
+    
+    // ambient
+    vec3 ambient = l.att.w * mat;
+
+    // diffuse
+    float dI = max(0.0, dot(normal, lightDir));
+    vec3 diff = dI * mat;
+    
+    // backfacing from the light?
+	if ( dI <= 0.0 ) return ambient + att * diff;
+
+    //specular
+    float sI = pow(max(0.0, dot(normalize(normal), halfN))), material.w);
+    vec3 spec = sI * l.spec * material.xyz;
+
+    //output color (no gamma correction)
+    return ambient + att * ( diff + spec );
+}
+
+void main()
+{
+    vec3 diffColor = texture( image, vUV );
+    for ( int i = 0; i<numLights; ++i )
+        FBColor.rgb += lighting( allLights[i], diffColor, finout.vNormal
+                                            , gl_Position, finout.vView );
+}
---
+++ vertex_cmap.glsl
@@ -0,0 +1,25 @@
+// Vertex shader for simple Phong shading with multiple Lights model
+#version 330
+
+uniform mat4 projM;
+uniform mat4 mvM[6];
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texCoord;
+layout (location = 3) in vec3 instpos;
+layout (location = 4) in vec3 instcolor;
+
+out VertexData {
+	smooth out vec2 vUV;
+	smooth out vec3 vNormal;
+	smooth out vec3 vView;
+} vout;
+
+void main(void)
+{
+	vout.vNormal = normal;
+	vout.vUV = texCoord;
+	// transform the geometry!
+	gl_Position = vec4( position, 1 );
+}
